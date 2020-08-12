90a2be14951fcbe179a9b291e23adf957fe75988
GabrielRechBrand <contatogabrielbrand@gmail.com>
2020-08-11 23:03:57 -0300

    Primeiro commit

---
+++ src/main/java/brand/Programa.java
@@ -0,0 +1,19 @@
+package main.java.brand;
+
+public class Programa {
+    public static void main(String[] args) {
+        for (int nome = 0; (nome <= 999); ++nome) {
+
+            if (nome % 3 == 0 && nome % 5 == 0) {
+                System.out.println(nome + "\tfizzbuzz");
+
+            } else if (nome % 3 == 0) {
+                System.out.println(nome + "\tfizz");
+
+            } else if (nome % 5 == 0) {
+                System.out.println(nome + "\tbuzz");
+
+            }
+        }
+    }
+}
